11d7ed529c3b0fc374e0356b2e9796411e7736e9
Adam Rush <adamrushuk@gmail.com>
2018-02-08 21:01:27 +0000

    Added all packer configs to Invoke-Packer wrapper script

--- Invoke-Packer.ps1
+++ Invoke-Packer.ps1
@@ -1,20 +1,23 @@
 # Vars
-#$packerConfigPath = '.\vb-win2012r2-base.json'
-#$packerConfigPath = '.\vb-win2012r2-powershell5.json'
-$packerConfigPath = '.\vb-win2012r2-export-vagrant.json'
+$packerConfigPaths = '.\vb-win2012r2-base.json', '.\vb-win2012r2-powershell5.json', '.\vb-win2012r2-export-vagrant.json'
 
-# Enable logging
-Write-Host 'Enabling logging...' -ForegroundColor Yellow
-$timestamp = Get-Date -Format 'yyyyMMdd-HHmmss'
-$env:PACKER_LOG = 1
-$env:PACKER_LOG_PATH = ".\logs\packer-$($timestamp).log"
-# Check environment vars
-Get-ChildItem env: | Where-Object Name -match packer
+# Start packer build for all configs
+foreach ($packerConfigPath in $packerConfigPaths) {
 
-# Check syntax
-Write-Host "`nChecking syntax for [$packerConfigPath]..." -ForegroundColor Yellow
-packer validate -syntax-only $packerConfigPath
+    # Enable logging
+    Write-Host 'Set logging environment variables...' -ForegroundColor Yellow
+    $timestamp = Get-Date -Format 'yyyyMMdd-HHmmss'
+    $env:PACKER_LOG = 1
+    $env:PACKER_LOG_PATH = ".\logs\packer-$($timestamp).log"
+    # Check environment vars
+    Get-ChildItem env: | Where-Object Name -match packer
 
-# Run Packer
-Write-Host "`nRunning Packer using [$packerConfigPath] config file..." -ForegroundColor Green
-packer build -on-error=abort $packerConfigPath
+    # Check syntax
+    Write-Host "`nChecking syntax for [$packerConfigPath]..." -ForegroundColor Yellow
+    packer validate -syntax-only $packerConfigPath
+
+    # Run Packer
+    Write-Host "`nRunning Packer using [$packerConfigPath] config file..." -ForegroundColor Green
+    packer build -on-error=abort $packerConfigPath
+
+}
