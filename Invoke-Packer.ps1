a6db9aabc072294478cfd3e90073c65e23eeba1d
Adam Rush <adamrushuk@gmail.com>
2019-09-22 17:05:23 +0100

    Added Azure Windows 2012 R2 example

--- Invoke-Packer.ps1
+++ Invoke-Packer.ps1
@@ -1,31 +1,55 @@
-# Vars
-#$packerConfigPaths = '.\vb-win2012r2-base.json', '.\vb-win2012r2-powershell5.json', '.\vb-win2012r2-export-vagrant.json'
-#$packerConfigPaths = '.\vb-win2012r2-base.json', '.\vb-win2012r2-wmf5-devtools.json', '.\vb-win2012r2-export-vagrant.json'
-#$packerConfigPaths = '.\vb-win2012r2-wmf5-devtools.json', '.\vb-win2012r2-export-vagrant.json'
-#$packerConfigPaths = '.\vb-win2012r2-export-vagrant.json'
+<#
+.SYNOPSIS
+    Invokes Packer with support for multiple config files
+.DESCRIPTION
+    Invokes Packer with support for multiple config files
+    Enables logging and starts a timer
+.PARAMETER Path
+    An array of Packer config paths
+.EXAMPLE
+    .\Invoke-Packer.ps1
 
-# Note Win2016 source repo for base OVF is: D:\Code\GitHub\windows_2016
-# $packerConfigPaths = '.\vb-win2016-devtools.json'
-# $packerConfigPaths = '.\vb-win2016-export-vagrant.json'
-$packerConfigPaths = '.\vb-win2016-devtools.json', '.\vb-win2016-export-vagrant.json'
+    Starts Packer using the default Azure image config, "azure-windows-2012r2.json"
+.EXAMPLE
+    $packerConfigPaths = "vb-win2012r2-base.json", "vb-win2012r2-powershell5.json", "vb-win2012r2-export-vagrant.json"
+    .\Invoke-Packer.ps1 -Path $packerConfigPaths
+
+    Loops through 3 configs in order shown. Each one building upon the previous.
+.NOTES
+    Author:  Adam Rush
+    Blog:    https://adamrushuk.github.io
+    GitHub:  https://github.com/adamrushuk
+    Twitter: @adamrushuk
+#>
+
+[CmdletBinding()]
+param (
+    [ValidateNotNullOrEmpty()]
+    [String[]]$Path = "azure-windows-2012r2.json"
+)
+
+# Start timer
+$timer = [Diagnostics.Stopwatch]::StartNew()
 
 # Start packer build for all configs
-foreach ($packerConfigPath in $packerConfigPaths) {
-
+foreach ($packerConfigPath in $Path) {
     # Enable logging
-    Write-Host 'Set logging environment variables...' -ForegroundColor Yellow
-    $timestamp = Get-Date -Format 'yyyyMMdd-HHmmss.fff'
+    Write-Host "Set logging environment variables..." -ForegroundColor "Yellow"
+    $timestamp = Get-Date -Format "yyyyMMdd-HHmmss.fff"
     $env:PACKER_LOG = 1
     $env:PACKER_LOG_PATH = ".\logs\packer-$($timestamp).log"
     # Check environment vars
-    Get-ChildItem env: | Where-Object Name -match packer
+    Get-ChildItem env: | Where-Object Name -match "packer"
 
     # Check syntax
-    Write-Host "`nChecking syntax for [$packerConfigPath]..." -ForegroundColor Yellow
+    Write-Host "`nChecking syntax for [$packerConfigPath]..." -ForegroundColor "Yellow"
     packer validate -syntax-only $packerConfigPath
 
     # Run Packer
-    Write-Host "`nRunning Packer using [$packerConfigPath] config file..." -ForegroundColor Green
+    Write-Host "`nRunning Packer using [$packerConfigPath] config file..." -ForegroundColor "Yellow"
     packer build -force $packerConfigPath
-
 }
+
+# Stop timer
+$timer.Stop()
+Write-Host "`nPacker build(s) complete after [$($timer.Elapsed.Minutes)m$($timer.Elapsed.Seconds)s]`n" -ForegroundColor "Green"
